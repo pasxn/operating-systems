508467d544dc9a71ab3e97c0c71e3afd438fc0c1
pasxn <pasansanjula.android@gmail.com>
2021-12-08 20:08:01 +0530

    execcvp test

--- multiprocessing/execcvp.c
+++ multiprocessing/execcvp.c
@@ -6,10 +6,9 @@
 #include <sys/wait.h>
 
 int main() {
-    char *cmd = "ls";
-    char *argv[3];
+    char* argv[3];
     argv[0] = "ls";
     argv[1] = "-la";
     argv[2] = NULL;
-    execvp(cmd, argv); 
+    execvp(argv[0], argv); 
 }
