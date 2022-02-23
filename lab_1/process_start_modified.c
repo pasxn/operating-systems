96ebca5be47cc805328b644b40b8b493be0bf97d
pasxn <pasansanjula.android@gmail.com>
2022-02-23 09:27:35 +0530

    not yet

--- lab_1/process_start_modified.c
+++ lab_1/process_start_modified.c
@@ -1,42 +1,62 @@
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main(){
+int main() {
     pid_t c_pid, pid;
     char ch;
+    int status;
     char text[100];
     int i=0;
     int numforks=0;
 
-    while(strcmp(text, "exit") && ch != EOF) {
-        printf(">");
-        while((ch=getchar())!=EOF){
-            switch(ch){
-                case '\n':
-                    text[i]='\0';
-                    printf("%s\n",text);
-                    printf(">");
-                    i=0;
+    printf("> ");
+    while((ch=getchar())!=EOF) {
+        switch(ch) {
+            case '\n':
+                text[i]='\0';
+     
+                if (strcpy(text, "exit") == 0) {
+                    exit(0);
+                }
+                
+                c_pid = fork();   
+
+                if(c_pid == 0) {
                     break;
-                default:
-                    text[i]=ch;
-                    i++;
-            }
+                }    
+                
+                numforks++;     
+                i=0;
+                printf("> ");
+
+                break;
+            default:
+                text[i]=ch;
+                i++;
         }
-
-        c_pid = fork();   // duplicate the parent
-        if(c_pid == 0){   // child code breaks. parent code continues
-            break;
-            sleep(2);         // sleep for 5 seconds
-            _exit(12);        // exit with status 2             
-        }  
-        numforks++;       // remember the number of child processes created     
     }
-    printf("\n");
-    return 0; //success
+    if( c_pid == 0 ) {    
+        printf("Child\n");
+        printf("User entered text: %s\n", text);
+        // sleep(5);        
+        _exit(2);        
+    }else if (c_pid > 0) {
+        printf("Parent\n");
+        while(numforks) { 
+            pid = wait(&status);
+            if ( WIFEXITED(status) ) {
+                printf("Parent: Child exited with status: %d\n",
+                                           WEXITSTATUS(status));
+            }
+            numforks--;
+        }
+    }else {        
+        perror("fork failed");
+        _exit(2); 
+    }
+    return 0; 
 }
-
