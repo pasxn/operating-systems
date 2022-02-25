c34c4bbc22af8ea82346d38cda53a5c96e07953a
pasxn <pasansanjula.android@gmail.com>
2022-02-25 20:05:17 +0530

    prompting

--- lab_1/process_start_modified.c
+++ lab_1/process_start_modified.c
@@ -19,44 +19,39 @@ int main() {
             case '\n':
                 text[i]='\0';
      
-                if (strcpy(text, "exit") == 0) {
+                if (strcmp(text, "exit") == 0) {
                     exit(0);
                 }
                 
                 c_pid = fork();   
-
-                if(c_pid == 0) {
-                    break;
-                }    
-                
                 numforks++;     
                 i=0;
-                printf("> ");
 
+                if(c_pid == 0) {
+                    printf("User entered text: %s\n", text);
+                    sleep(5);        
+                    _exit(2);      
+                    break;
+                } else if (c_pid > 0) {
+                    while(numforks) { 
+                        pid = wait(&status);
+                        if ( WIFEXITED(status) ) {
+                            // printf("Parent: Child exited with status: %d\n",WEXITSTATUS(status));
+                        }
+                        printf("> ");
+                        numforks--;
+                    }
+                } else {        
+                    perror("fork failed");
+                    _exit(2); 
+                }    
                 break;
+
             default:
                 text[i]=ch;
                 i++;
         }
     }
-    if( c_pid == 0 ) {    
-        printf("Child\n");
-        printf("User entered text: %s\n", text);
-        // sleep(5);        
-        _exit(2);        
-    }else if (c_pid > 0) {
-        printf("Parent\n");
-        while(numforks) { 
-            pid = wait(&status);
-            if ( WIFEXITED(status) ) {
-                printf("Parent: Child exited with status: %d\n",
-                                           WEXITSTATUS(status));
-            }
-            numforks--;
-        }
-    }else {        
-        perror("fork failed");
-        _exit(2); 
-    }
+    printf("\n");
     return 0; 
 }
