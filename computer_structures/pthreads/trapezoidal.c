1635eb52b6d8f005feef276ebad4274205ae7e07
pasxn <pasansanjula.android@gmail.com>
2023-10-02 12:52:24 +0530

    trapezoidal

--- computer_structures/pthreads/trapezoidal.c
+++ computer_structures/pthreads/trapezoidal.c
@@ -28,16 +28,16 @@
 #include <semaphore.h>
 
 /* The global variables are shared among all the threads. */
-int    thread_count;
+int thread_count;
+int n, local_n;
+int method;
 double a, b, h;
-int    n, local_n;
-int	method;
 
 /* Critical section lock variables */
 int flag;
 sem_t sem;
 pthread_mutex_t mutex;
-double  total;
+double total;
 
 void* thread_work(void* rank);
 
@@ -49,8 +49,8 @@ double f(double x);
 
 /*--------------------------------------------------------------*/
 int main(int argc, char** argv) {
-  long        i;
-  pthread_t*  thread_handles;  
+  long i;
+  pthread_t*  thread_handles;
 
   total = 0.0;
   if (argc != 3) {
@@ -76,11 +76,13 @@ int main(int argc, char** argv) {
   /* Start the threads. */
   for (i = 0; i < thread_count; i++) {
     /* create thread with attribute thread_handle[i], executing the function thread_work, with rank i */
+    pthread_create(&thread_handles[i], NULL, thread_work, &i);
   }
 
   /* Wait for threads to complete. */
   for (i = 0; i < thread_count; i++) {
     /* join corresponding threads */
+    pthread_join(thread_handles[i], NULL);
   }
 
   printf("With n = %d trapezoids, our estimate\n", n);
@@ -95,13 +97,12 @@ int main(int argc, char** argv) {
 
 /*--------------------------------------------------------------*/
 void* thread_work(void* rank) {
-  double  local_a;   /* Left endpoint my thread   */
-  double  local_b;   /* Right endpoint my thread  */
-  double  my_int;    /* Integral over my interval */
-  long    my_rank = (long) rank;
+  double local_a;   /* Left endpoint my thread   */
+  double local_b;   /* Right endpoint my thread  */
+  double  my_int;   /* Integral over my interval */
+  long my_rank = *(long*)(rank);
 
-  /* Length of each process' interval of integration = */
-  /* local_n*h.  So my interval starts at:             */
+  /* Length of each process' interval of integration = local_n*h.  So my interval starts at: */
   local_a = a + my_rank * local_n * h;
   local_b = local_a + local_n * h;
 
@@ -110,12 +111,21 @@ void* thread_work(void* rank) {
   switch (method) {
     case 2:
 	    /* semaphore critical section to add local integral to total */
+      sem_wait(&sem);
+      total =+ my_int;
+      sem_post(&sem);
 	    break;
 	  case 3:
 	    /* busy-wait critical section to add local integral to total */
+      while (__sync_lock_test_and_set(&flag, 1) == 1);
+      total =+ my_int;
+      __sync_lock_release(&flag);
 	    break;
 	  default:
 	    /* mutex critical section to add local integral to total */
+      pthread_mutex_lock(&mutex);
+      total =+ my_int;
+      pthread_mutex_unlock(&mutex);
 	    break;
     }
     
