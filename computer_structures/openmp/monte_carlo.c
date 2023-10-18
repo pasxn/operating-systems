5c873ec727aab003a4df6f0782cf795dab616dbd
pasxn <pasansanjula.android@gmail.com>
2023-10-18 17:45:23 +0530

    fix dumb impl

--- computer_structures/openmp/monte_carlo.c
+++ computer_structures/openmp/monte_carlo.c
@@ -4,9 +4,10 @@
 
 #define NUM_THREADS 8
 
+long long int local_number_in_circle_per_thread[NUM_THREADS] = {0};
 
 long long int Parallelize(long long int local_number_of_tosses);
-long long int Monte_carlo(long long number_of_tosses, int tid);
+void Monte_carlo(long long number_of_tosses, int tid);
 
 int main(void) {
   long long int number_of_tosses;
@@ -29,7 +30,7 @@ int main(void) {
   return 0;
 }
 
-long long int Monte_carlo(long long local_number_of_tosses, int tid) {
+void Monte_carlo(long long local_number_of_tosses, int tid) {
   long long int i;
   double x, y;
   double distance_squared;
@@ -41,16 +42,18 @@ long long int Monte_carlo(long long local_number_of_tosses, int tid) {
     x = (double)random()/RAND_MAX; /* x= random between -1 and 1 */
     y = (double)random()/RAND_MAX; /* y= random between -1 and 1 */
 
-    distance_squared = (x*x) + (y*y) ;
+    distance_squared = (x*x) + (y*y);
     
     /* distance_squared = distance squared of dart toss from centre position */
-    if(distance_squared <= 1){
+    if(distance_squared <= 1) {
       /* if dart falls in unit circle, increment the count local_number_in_circle */
       local_number_in_circle += 1;
     }
   }
 
-  return local_number_in_circle;
+  printf("number of tosses: %lld process id: %d\n", local_number_in_circle, tid);
+
+  local_number_in_circle_per_thread[tid] = local_number_in_circle;
 }
 
 long long int Parallelize(long long int local_number_of_tosses) {
@@ -59,8 +62,12 @@ long long int Parallelize(long long int local_number_of_tosses) {
 
   #pragma omp parallel num_threads(NUM_THREADS)
   {
+    int tid = omp_get_thread_num();
+
+    Monte_carlo(local_number_of_tosses, tid);
+
     #pragma omp critical
-    number_in_circle += Monte_carlo(local_number_of_tosses, omp_get_thread_num());
+    number_in_circle += local_number_in_circle_per_thread[tid];
   }
 
   return number_in_circle;
